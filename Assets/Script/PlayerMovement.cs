184075beaec8a7d72e244ef5e4090dd80c847378
UnMaxdeUwU <quentinbres3@gmail.com>
2025-10-09 15:04:18 +0200

    grappin

--- Assets/Script/PlayerMovement.cs
+++ Assets/Script/PlayerMovement.cs
@@ -1,3 +1,4 @@
+using System.Net.NetworkInformation;
 using UnityEngine;
 [RequireComponent(typeof(Rigidbody))]
 public class PlayerMovement : MonoBehaviour
@@ -6,7 +7,10 @@ public class PlayerMovement : MonoBehaviour
     private float _horizontalMovement;
     private float _verticalMovement;
     private Vector3 _movement;
+    private Vector3 _grappinDirection;
+    private Vector3 _grappinHit;
     [SerializeField] private float _speed = 10f;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -20,6 +24,7 @@ public class PlayerMovement : MonoBehaviour
         _horizontalMovement = Input.GetAxis("Horizontal");
         _verticalMovement = Input.GetAxis("Vertical");
         _movement = new Vector3(_horizontalMovement, 0, _verticalMovement);
+        GrappinUpdateDirection(_movement); //mise a jour direction grappin
         _movement.Normalize();
         _movement.y = _rb.linearVelocity.y;
         if ( _rb != null)
@@ -32,5 +37,38 @@ public class PlayerMovement : MonoBehaviour
             Debug.LogError("no rigibody attached");
         }
 
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            TryThrowGrappin();
+            
+        }
+
+        if (Input.GetKeyUp(KeyCode.G))
+        {
+            ThrowGrappin();
+        }
+    }
+
+    private void GrappinUpdateDirection(Vector3 direction)
+    {   
+        if (direction.sqrMagnitude > 0.1f)
+        {
+            _grappinDirection = direction;
+        }
+    }
+
+    private void TryThrowGrappin()
+    {   
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, _grappinDirection, out hit, 100f))
+        {
+            _grappinHit = hit.point+hit.normal*1.5f;
+        }
+    }
+
+    private void ThrowGrappin()
+    {
+        transform.position = _grappinHit;
+        _grappinDirection = Vector3.zero;
     }
 }
